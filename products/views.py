847c10ad96f0c839aeefb5ed4c38d6e3582505a8
ansoncodes <ansonantony.work@gmail.com>
2025-12-06 15:26:57 +0530

    added a comment

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-
+#hello
 @api_view(['GET', 'POST'])
 def product_list(request):
     """
